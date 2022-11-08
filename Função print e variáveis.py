85aa794d7c8f0e16465646ffa6842298b08e6658
Martinez <104662583+Martiinez@users.noreply.github.com>
2022-11-08 18:24:20 -0300

    Importação de jogos de python

---
+++ Função print e variáveis.py
@@ -0,0 +1,25 @@
+print("Ola Mundo");
+
+print("Brasil", "ganhou", 5 , "titulos mundiais", sep="-");
+print("Brasil", "ganhou", 5 , "titulos mundiais", end="\n");
+
+pais = "Italia";
+quantidade = 4;
+
+print(pais, "ganhou",quantidade,"titulos mundiais");
+
+paisDois = "Alemanha";
+titulos = 4;
+
+print(paisDois, "ganhou", titulos,"titulos mundiais");
+
+subst = "Python"
+verbo = "é"
+adjetivo = "fantástico"
+print(subst, verbo, adjetivo, sep="_", end="!\n")
+
+dia = 15;
+mes = 10;
+ano = 2015;
+
+print(dia,mes,ano, sep="/");
---
+++ formatacao de strings.py
@@ -0,0 +1,11 @@
+print("Tentativa {} de {}" . format(3,10))
+
+print("Tentativa {1} de {0}" . format(3,10))
+
+print("R$: {:07.2f}" . format(1.5))
+
+print("R$: {:07d}" . format(4))
+
+print("Data: {:02d}/{:02d}". format(19,11))
+
+print(3 // 2)
